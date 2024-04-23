f3eef3827fcff8f6c46c6442fcf58e4a66026292
Sidharth Kathpal <33653824+sidharthkathpal@users.noreply.github.com>
2024-04-23 11:59:08 +0530

    renaming to search

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-from search_src import __version__
+from search import __version__
 
 setup(
-    name='search_src',
+    name='search',
     version=__version__,
 
     url='https://github.com/GovML/retriever.git',
     author='Sidharth Kathpal',
     author_email='kathpal.sid@gmail.com',
 
-    py_modules=['search_src'],
+    py_modules=['search'],
 )
