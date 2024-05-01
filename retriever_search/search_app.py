4547a987498d7493d5bf1fa96bea3cb5d5d0cdb4
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2024-04-30 20:21:11 -0700

    made model in init

--- retriever_search/search_app.py
+++ retriever_search/search_app.py
@@ -6,20 +6,21 @@ from haystack_integrations.document_stores.qdrant import QdrantDocumentStore
 
 
 class search_app:
-    def __init__(self, inputDirectory, json_save_path, embedding_2d = "red_emb_1K.npy", device = 'cpu'):
+    def __init__(self, inputDirectory, json_save_path, embedding_2d = "red_emb_1K.npy", device = 'cpu', model = 'sentence-transformers/allenai-specter'):
         self.embedding_2d = embedding_2d
         self.json_path = json_save_path
         self.inputDirectory = inputDirectory
         self.device = device
+        self.model = model
 
     def new_search_run(self):
         #model = 'sentence-transformers/all-mpnet-base-v2'
-        model = 'sentence-transformers/allenai-specter'
+        #model = 'sentence-transformers/allenai-specter'
         #model = 'Dagar/t5-small-science-papers'
         if os.path.exists(self.json_path):
             ingestion = DocumentIngestion(self.json_path, model = None, device=self.device)
         else:
-            ingestion = DocumentIngestion(self.inputDirectory, model = model, device=self.device)
+            ingestion = DocumentIngestion(self.inputDirectory, model = self.model, device=self.device)
             ingestion.to_json(self.json_path)
 
         document_store = document_store = QdrantDocumentStore(
@@ -31,7 +32,7 @@ class search_app:
 
         pipe = QueryPipeline(document_store, 
                             type = 'Search', 
-                            ann_model=model, 
+                            ann_model=self.model, 
                             ranker_model="cross-encoder/ms-marco-MiniLM-L-12-v2"
                             )
         return pipe
