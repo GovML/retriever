67d2f324182ae79fcfb00971366b8c9025652226
Sidharth Kathpal <33653824+sidharthkathpal@users.noreply.github.com>
2024-07-12 17:08:19 -0700

    pushing the latest UI

--- retriever_search/search_ui.py
+++ retriever_search/search_ui.py
@@ -7,7 +7,7 @@ class GradioSearchUI:
         self.search_server_url =  search_server_url
         self.analysis_server_url = analysis_server_url
         self.current_results = pd.DataFrame([])
-        # self.previous_queries = []  # is a list of queries, not buttons
+        self.previous_queries = []  # is a list of queries, not buttons
         self.interface = self.build_interface()
         
     def search_query(self, query):
@@ -24,39 +24,48 @@ class GradioSearchUI:
                     "Content": doc["content"],
                 })
             self.current_results = pd.DataFrame(data)
-            return gr.DataFrame(self.current_results, visible = True, wrap=False, column_widths = ['30%','60%','10%']), synthesized_qa, gr.DownloadButton(label=f"Download Results", visible=True)    
+            
+            if [query] not in self.previous_queries:
+                self.previous_queries.append([query])
+            
+            return gr.DataFrame(self.current_results, visible = True, wrap=False, column_widths = ['30%','60%','10%']), synthesized_qa, gr.DownloadButton(label=f"Download Results", visible=True), gr.Dataset(samples=self.previous_queries, components=[self.search_input], type="index", label="Queries")   
         else:
-            return gr.DataFrame(pd.DataFrame([], columns = ['No Results Returned.']), visible = True), "### No Results Found", gr.DownloadButton(label=f"Download Results", visible=False)
+            return gr.DataFrame(pd.DataFrame([], columns = ['No Results Returned.']), visible = True), "### No Results Found", gr.DownloadButton(label=f"Download Results", visible=False), gr.Dataset(samples=self.previous_queries, components=[self.search_input], type="index", label="Queries")
     
     def download_results(self, query):
         sanitized_query = "".join(x for x in query if x.isalnum())
         gr.Info(f'Saving CSV {sanitized_query}.csv to current directory.')
         self.current_results.to_csv(f"./{sanitized_query}.csv")
 
-    # def update_previous_queries(self):
-    #     # Convert previous queries into Gradio buttons
-    #     response = requests.get(f'{self.search_server_url}/get_previous_queries')
-    #     if response.status_code == 200:
-    #         self.previous_queries = response.json()
-    #     else:
-    #         print('WARNING: No previous queries found. If this is your first query please ignore this warning.')
-    #         self.previous_queries = []
+    def update_previous_queries(self, id):
+        # Convert previous queries into Gradio buttons
+        input_query = self.previous_queries[id][0]
+        search_output, qa_output, _, _ = self.search_query(input_query)
+        return input_query, search_output, qa_output
 
     def build_interface(self):
         with gr.Blocks() as interface:
-            with gr.Tab("Search"):
+            with gr.Tab("SEARCH"):
+                gr.Markdown("# Let's Get You Started With Search")
                 self.search_input = gr.Textbox(placeholder="What would you like to know?", label="")
                 self.search_button = gr.Button("Search")
-                self.qa_output = gr.Markdown("### Synthesized Outputs will appear here.")
+
+                gr.Markdown("## Searched Queries")
+                self.searched_queries = gr.Dataset(samples=self.previous_queries, components=[self.search_input], type="index", label="Queries")
+
+                gr.Markdown("## Synthesized Outputs")
+                self.qa_output = gr.Markdown("### Outputs will appear here.")
                 self.search_output = gr.DataFrame(self.current_results, visible = True)
                 self.download_button = gr.DownloadButton(label=f"Download Results", visible=False)
-                            
-                self.search_button.click(fn=self.search_query, inputs=self.search_input, outputs=[self.search_output, self.qa_output, self.download_button])
+                
+
+                self.searched_queries.click(self.update_previous_queries, inputs=[self.searched_queries], outputs=[self.search_input, self.search_output, self.qa_output])
+                self.search_button.click(fn=self.search_query, inputs=self.search_input, outputs=[self.search_output, self.qa_output, self.download_button, self.searched_queries])
                 self.download_button.click(fn = self.download_results, inputs = self.search_input)
                     
-            with gr.Tab("Analysis"):
+            with gr.Tab("ANALYSIS"):
                 # This tab is left blank for now
-                gr.Markdown("Analysis Tab Content")
+                gr.Markdown("# Analysis Tab Content")
                 src_val = self.analysis_server_url
                 html = ("<iframe id=\"iframeid\" src=" + src_val + "\ width=1200 height=2000>")
                 out1 = gr.HTML(html)
