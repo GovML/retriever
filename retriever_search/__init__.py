ebbbca0dd2341fcba8431006888983ffff4db406
Sidharth Kathpal <33653824+sidharthkathpal@users.noreply.github.com>
2024-08-11 19:07:28 -0700

    updating the init.py

--- retriever_search/__init__.py
+++ retriever_search/__init__.py
@@ -1,10 +1,6 @@
-__version__ = '0.5.0'
+__version__ = '1.0.0'
 
 
 def hello_world():
-    print("This is my first pip package!")
-
-
-# python3 -m build 
-# twine upload dist/* --verbose
+    print("welcome to your local search server")
     
